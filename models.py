5aab3921e8e1497daf3fc4c50c78e244307c0a1e
Sanidhya <mangalsanidhya19@gmail.com>
2021-11-01 14:05:37 -0500

    feat: added rotnet model
    added: trainer script for rotnet model

---
+++ models.py
@@ -0,0 +1,40 @@
+"""
+author:Sanidhya Mangal
+github:sanidhyamangal
+"""
+
+from typing import List, Tuple
+import tensorflow as tf  # for deep learning tasks
+# use resnet50 model for this task
+from tensorflow.keras.applications.resnet_v2 import ResNet50V2, preprocess_input
+
+
+class RotationNetModel(tf.keras.models.Model):
+    preprocess_inputs = preprocess_input
+
+    def __init__(self,
+                 image_shape: Tuple[int] = (244, 244, 3),
+                 num_classes: int = 4,
+                 num_hidden_units: List[int] = [512, 512],
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.base_model = ResNet50V2(weights=None,
+                                     include_top=False,
+                                     input_shape=image_shape)
+
+        _clf_layers = [
+            tf.keras.layers.Dense(hidden) for hidden in num_hidden_units
+        ]
+        _clf_layers.append(tf.keras.layers.Dense(num_classes))
+        self.classifier = tf.keras.models.Sequential(_clf_layers)
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.preprocess_inputs(inputs, training=training)
+
+        feature_extractor = self.base_model(inputs)
+
+        x = self.classifier(feature_extractor)
+
+        return x
---
+++ train_rotnet.py
@@ -0,0 +1,26 @@
+"""
+author:Sanidhya Mangal
+github:sanidhyamangal
+"""
+
+import os # for os related ops
+import argparse # for argument parsing
+import tensorflow as tf # for deep learning ops
+from models import RotationNetModel
+
+
+TRAIN_SET = "/data/maiziezhou_lab/sanidhya/rotnet/rotated_data_train"
+TEST_SET = "/data/maiziezhou_lab/sanidhya/rotnet/rotated_data_test"
+
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(TRAIN_SET, image_size=(244,244), batch_size=64)
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(TEST_SET, image_size=(244,244),batch_size=64)
+
+
+rotnet = RotationNetModel()
+
+rotnet.compile(loss=[tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)],metrics=["accuracy"])
+
+tb_callback = tf.keras.callbacks.TensorBoard(log_dir="rotnet")
+ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath="rotnet_models", save_weights_only=True)
+
+rotnet.fit_generator(train_dataset, epochs=500, callbacks=[tb_callback, ckpt_callback])
