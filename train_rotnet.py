fa937473a4060920c9db49d5dccc09c23470af8b
Sanidhya <mangalsanidhya19@gmail.com>
2021-11-01 14:22:13 -0500

    fix: replaced data loading pipeline

--- train_rotnet.py
+++ train_rotnet.py
@@ -7,15 +7,35 @@ import os # for os related ops
 import argparse # for argument parsing
 import tensorflow as tf # for deep learning ops
 from models import RotationNetModel
+from datapipeline.load_imageds import LoadData
 
 
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 TRAIN_SET = "/data/maiziezhou_lab/sanidhya/rotnet/rotated_data_train"
 TEST_SET = "/data/maiziezhou_lab/sanidhya/rotnet/rotated_data_test"
 
-image_data = tf.keras.preprocessing.image.ImageDataGenerator()
-train_dataset = image_data.flow_from_directory(TRAIN_SET, target_size=(244,244), batch_size=64)
-test_dataset = image_data.flow_from_directory(TEST_SET, target_size=(244,244),batch_size=64)
 
+
+train_dataset_loader =LoadData(path=TRAIN_SET,
+                 image_shape=(244,244),
+                 channel=3)
+
+val_dataset_loader = LoadData(path=TEST_SET,
+                image_shape=(244,244),
+                channel=3)
+
+train_dataset = train_dataset_loader.create_dataset(
+        batch_size=64,
+        autotune=AUTOTUNE,
+        drop_remainder=True,
+        prefetch=True)
+
+# prepare validation dataset for the ingestion process
+validation_dataset = val_dataset_loader.create_dataset(
+    batch_size=64,
+    autotune=AUTOTUNE,
+    drop_remainder=True,
+    prefetch=True)
 
 rotnet = RotationNetModel()
 
@@ -24,4 +44,4 @@ rotnet.compile(loss=[tf.keras.losses.SparseCategoricalCrossentropy(from_logits=T
 tb_callback = tf.keras.callbacks.TensorBoard(log_dir="rotnet",histogram_freq=1)
 ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath="rotnet_models", save_weights_only=True)
 
-rotnet.fit(train_dataset, validation_data=test_dataset,epochs=500, callbacks=[tb_callback, ckpt_callback])
+rotnet.fit(train_dataset, validation_data=validation_dataset,epochs=500, callbacks=[tb_callback, ckpt_callback])
