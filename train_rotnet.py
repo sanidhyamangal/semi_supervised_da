da49ad6be5993809f978c398163be45161bb6002
Sanidhya <mangalsanidhya19@gmail.com>
2021-11-01 14:12:00 -0500

    fixed: model fitting path

--- train_rotnet.py
+++ train_rotnet.py
@@ -13,14 +13,14 @@ TRAIN_SET = "/data/maiziezhou_lab/sanidhya/rotnet/rotated_data_train"
 TEST_SET = "/data/maiziezhou_lab/sanidhya/rotnet/rotated_data_test"
 
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(TRAIN_SET, image_size=(244,244), batch_size=64)
-train_dataset = tf.keras.preprocessing.image_dataset_from_directory(TEST_SET, image_size=(244,244),batch_size=64)
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(TEST_SET, image_size=(244,244),batch_size=64)
 
 
 rotnet = RotationNetModel()
 
 rotnet.compile(loss=[tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)],metrics=["accuracy"])
 
-tb_callback = tf.keras.callbacks.TensorBoard(log_dir="rotnet")
+tb_callback = tf.keras.callbacks.TensorBoard(log_dir="rotnet",histogram_freq=1)
 ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath="rotnet_models", save_weights_only=True)
 
-rotnet.fit_generator(train_dataset, epochs=500, callbacks=[tb_callback, ckpt_callback])
+rotnet.fit(train_dataset, validation_data=test_dataset,epochs=500, callbacks=[tb_callback, ckpt_callback])
