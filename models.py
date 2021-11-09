99c0fd70c4f6aed33acc670630ff5cc83164e0e2
Sanidhya <mangalsanidhya19@gmail.com>
2021-11-09 01:46:02 -0600

    feat: added pac model

--- models.py
+++ models.py
@@ -9,12 +9,49 @@ import tensorflow as tf  # for deep learning tasks
 from tensorflow.keras.applications.resnet_v2 import preprocess_input
 
 
-class RotationNetModel(tf.keras.models.Model):
+# class RotationNetModel(tf.keras.models.Model):
+
+#     def __init__(self,
+#                  image_shape: Tuple[int] = (244, 244, 3),
+#                  num_classes: int = 4,
+#                  num_hidden_units: int = 256,
+#                  *args,
+#                  **kwargs):
+#         super().__init__(*args, **kwargs)
+
+#         self.base_model = tf.keras.applications.ResNet50V2(include_top=False,
+#                                      input_shape=image_shape)
+
+#         _clf_layers = [
+#             tf.keras.layers.GlobalAveragePooling2D(),
+#             tf.keras.layers.Dense(num_hidden_units),
+#             tf.keras.layers.Dropout(0.3),
+#             tf.keras.layers.Dense(num_hidden_units),
+#             tf.keras.layers.Dropout(0.3),
+#             tf.keras.layers.Dense(num_classes)
+#         ]
+
+#         self.classifier = tf.keras.models.Sequential(_clf_layers)
+
+#     def call(self, inputs, training=None, mask=None):
+#         x = preprocess_input(inputs)
+
+#         feature_extractor = self.base_model(x, training=training)
+
+#         x = self.classifier(feature_extractor, training=training)
+    
+#         return x
+
+class PAC(tf.keras.models.Model):
+    """
+    Model for performing PAC
+    """
 
     def __init__(self,
                  image_shape: Tuple[int] = (244, 244, 3),
-                 num_classes: int = 4,
-                 num_hidden_units: int = 256,
+                 num_classes: int = 65,
+                 num_hidden_units: List[int] = [256,256],
+                 dropout:float=0.3,
                  *args,
                  **kwargs):
         super().__init__(*args, **kwargs)
@@ -22,13 +59,18 @@ class RotationNetModel(tf.keras.models.Model):
         self.base_model = tf.keras.applications.ResNet50V2(include_top=False,
                                      input_shape=image_shape)
 
+        _clf_hidden_layer_stack = []
+        
+        for hidden_states in num_hidden_units:
+            _clf_hidden_layer_stack.extend([
+                tf.keras.layers.Dense(hidden_states, activation="relu"),
+                tf.keras.layers.Dropout(dropout)
+            ])
+
         _clf_layers = [
             tf.keras.layers.GlobalAveragePooling2D(),
-            tf.keras.layers.Dense(num_hidden_units),
-            tf.keras.layers.Dropout(0.3),
-            tf.keras.layers.Dense(num_hidden_units),
-            tf.keras.layers.Dropout(0.3),
-            tf.keras.layers.Dense(num_classes)
+            *_clf_hidden_layer_stack,
+            tf.keras.layers.Dense(num_classes, activation="softmax")
         ]
 
         self.classifier = tf.keras.models.Sequential(_clf_layers)
