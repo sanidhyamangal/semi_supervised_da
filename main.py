30e9c5cff93aaef31697852e7722cc757fb0ade0
Sanidhya <mangalsanidhya19@gmail.com>
2021-11-25 01:14:07 -0600

    feat: added unsupervised trainer
    added: runner script for the training ops

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from datapipeline.load_imageds import (  # model pipeline for loading image data
     LoadData, PredictionDataLoader)
 from logger import logger  # for logging
 from models import PAC
-from trainer import BaseTrainer  # model manager for handing all the ops
+from trainer import BaseTrainer, UnsupervisedTrainer  # model manager for handing all the ops
 
 
 def train_model(args) -> None:
@@ -102,6 +102,72 @@ def train_model(args) -> None:
                   DATA_DICT["target"]["dataset"],
                   DATA_DICT["unlabeled"]["dataset"], args.path_to_save_weights)
 
+def train_model_unsupervised(args) -> None:
+    """
+    Helper function for train arg subparser to train the entire network
+    """
+    # define data loader for the validation and trainer set
+    source_dataset_loader = [
+        LoadData(path=_path,
+                 image_shape=(args.height, args.width),
+                 channel=args.channel) for _path in args.path_to_source_dir
+    ]
+
+    unlabeled_dataset_loader = [
+        LoadData(path=_path,
+                 image_shape=(args.height, args.width),
+                 channel=args.channel) for _path in args.path_to_unlabeled_dir
+    ]
+
+    logger.info(f"PRETRAINED WEIGHTS: {args.path_to_pretrained_weights}")
+
+    # retrieve and define the model for the interconnection
+    model = PAC(image_shape=(args.height, args.width, args.channel),
+                num_hidden_units=[512, 512],
+                num_classes=len(source_dataset_loader[0].root_labels),
+                weights=args.path_to_pretrained_weights)
+
+    DATA_DICT = {
+        "source": {
+            "dataloader":
+            source_dataset_loader,
+            "dataset":
+            source_dataset_loader[0].create_dataset(batch_size=args.batch_size,
+                                                    autotune=AUTOTUNE,
+                                                    drop_remainder=True,
+                                                    prefetch=True)
+        },
+        "unlabeled": {
+            "dataloader":
+            unlabeled_dataset_loader,
+            "dataset":
+            unlabeled_dataset_loader[0].create_dataset(
+                batch_size=args.batch_size,
+                autotune=AUTOTUNE,
+                drop_remainder=True,
+                prefetch=True,
+                pertubed_images=True)
+        }
+    }
+
+    for scope in DATA_DICT:
+        if len(DATA_DICT[scope]["dataloader"]) > 1:
+            for i in DATA_DICT[scope]["dataloader"][1:]:
+                DATA_DICT[scope]["dataset"].concatenate(
+                    i.create_dataset(batch_size=args.batch_size,
+                                     autotune=AUTOTUNE,
+                                     drop_remainder=True,
+                                     prefetch=True,
+                                     pertubed_images=scope == "unlabeled"))
+
+    trainer = UnsupervisedTrainer(model,
+                          optimizer=tf.keras.optimizers.Adam(args.lr),
+                          log_file_name=args.log_file_path,
+                          num_classes=len(
+                              source_dataset_loader[0].root_labels))
+
+    trainer.train(args.epoch, DATA_DICT["source"]["dataset"],
+                  DATA_DICT["unlabeled"]["dataset"], args.path_to_save_weights)
 
 if __name__ == "__main__":
     AUTOTUNE = tf.data.AUTOTUNE
@@ -112,6 +178,8 @@ if __name__ == "__main__":
     parser_semi = subparsers.add_parser(
         'semi',
         help='train the classification model in semi supervised fashion')
+    parser_un = subparsers.add_parser("unsupervised", help='train the classification model in semi supervised fashion')
+
     parser_predict = subparsers.add_parser(
         'predict', help='make predications for candidate SVs')
 
@@ -183,6 +251,70 @@ if __name__ == "__main__":
         dest="channel")
 
     parser_semi.set_defaults(func=train_model)
+
+    parser_un.add_argument('--epoch',
+                             type=int,
+                             default=2,
+                             help='number of total epoches',
+                             dest="epoch")
+    parser_un.add_argument('--batch_size',
+                             type=int,
+                             default=32,
+                             help='number of samples in one batch',
+                             dest="batch_size")
+    parser_un.add_argument('--lr',
+                             type=float,
+                             default=1e-3,
+                             help='initial learning rate for adam',
+                             dest="lr")
+    parser_un.add_argument('--path_to_source_dir',
+                             required=True,
+                             help='path to source dataset directory',
+                             dest="path_to_source_dir",
+                             action="append")
+    parser_un.add_argument('--path_to_unlabeled_dir',
+                             required=True,
+                             help='path to unlabeled dataset directory',
+                             dest="path_to_unlabeled_dir",
+                             action="append")
+
+    parser_un.add_argument(
+        '--path_to_pretrained_weights',
+        help='path to directory from where pretrained weights needs to be loaded, default to rotnet',
+        dest="path_to_pretrained_weights",
+        default="pretrained_models/rotnet.h5")
+    parser_un.add_argument(
+        '--path_to_save_weights',
+        required=True,
+        help='path to directory where checkpoints needs to be saved',
+        dest="path_to_save_weights",
+        default="pretrained_models/rotnet.h5")
+    parser_un.add_argument(
+        '--log_file_path',
+        required=True,
+        help='file name to save the model training loss in csv file',
+        dest="log_file_path")
+
+    parser_un.add_argument(
+        "--height",
+        default=224,
+        type=int,
+        help="height of input images, default value is 224",
+        dest="height")
+    parser_un.add_argument(
+        "--width",
+        default=224,
+        type=int,
+        help="width of input images, default value is 224",
+        dest="width")
+    parser_un.add_argument(
+        "--channel",
+        default=3,
+        type=int,
+        help="channel of input images, default value is 3",
+        dest="channel")
+
+    parser_un.set_defaults(func=train_model_unsupervised)
 
     args = parser.parse_args()
     args.func(args)
--- trainer.py
+++ trainer.py
@@ -108,3 +108,63 @@ class BaseTrainer:
     def save_weights(self, path: str) -> None:
         create_folders_if_not_exists(path)  # save path
         self.model.save_weights(path)  # save weights
+
+class UnsupervisedTrainer(BaseTrainer):
+    def train_step(self, source_batch, unlabeled_batch):
+        loss = 0
+        with tf.GradientTape() as tape:
+            if source_batch.has_value():
+                imgs, labels = source_batch.get_value()
+                pred = self.model(imgs)
+                loss += tf.reduce_mean(
+                    compute_h(tf.one_hot(labels, depth=self.num_classes),
+                              pred))
+
+            imgs = unlabeled_batch.get_value()
+            pertubed_imgs = GeneratePertuberations(imgs)
+
+            px = self.model(imgs)
+            qx = self.model(pertubed_imgs)
+
+            loss += compute_cr(px, qx, 0.9)
+
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(
+            zip(grads, self.model.trainable_variables))
+
+        return loss
+
+    def train(self,
+              epochs: int,
+              source_dataset,
+              unlabeled_dataset,
+              weights_path="str"):
+
+        for i in range(epochs):
+            # call training step
+            epoch_loss = []
+            source_iterator = iter(source_dataset)
+            unlabeled_iterator = iter(unlabeled_dataset)
+
+            while True:
+                source_batch = source_iterator.get_next_as_optional()
+                unlabeled_batch = unlabeled_iterator.get_next_as_optional()
+
+                if not unlabeled_batch.has_value():
+                    break
+
+                loss = self.train_step(source_batch,
+                                       unlabeled_batch)
+                logger.info(f"Batch Loss: {loss}")
+                epoch_loss.append(loss)
+                self.write_logs_csv(loss)
+
+            ep_loss = np.mean(epoch_loss)
+            logger.info(f"Epoch: {i},Loss:{ep_loss}")
+
+            if loss < self.base_loss:
+                self.save_weights(weights_path)
+                self.base_loss = loss
+                logger.info(f"Saving Weights at Epoch :{i} - Loss:{loss}")
+        
+        logger.info("Training Finished !!!!")
